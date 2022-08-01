8874fa4c6f7675fe6322244aaaf7a2c0ef5ffb6f
朱君明 <zhujunming@yunquna.com>
2022-08-01 11:16:08 +0800

    fix

--- mybatis-plus-generator/src/test/java/com/baomidou/mybatisplus/generator/samples/YqnGeneratorTest.java
+++ mybatis-plus-generator/src/test/java/com/baomidou/mybatisplus/generator/samples/YqnGeneratorTest.java
@@ -1,8 +1,10 @@
 package com.baomidou.mybatisplus.generator.samples;
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.generator.FastAutoGenerator;
 import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
 import com.baomidou.mybatisplus.generator.config.OutputFile;
+import com.baomidou.mybatisplus.generator.config.StrategyConfig;
 import com.baomidou.mybatisplus.generator.config.rules.DateType;
 import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
 
@@ -15,7 +17,7 @@ import java.util.Collections;
 public class YqnGeneratorTest {
 
     public static void main(String[] args) {
-        FastAutoGenerator.create(new DataSourceConfig.Builder("jdbc:mysql://mysql-common.qa-pub.yqn.corp:3306/yqn_composer_v2", "root", "sr@12345"))
+        FastAutoGenerator.create(new DataSourceConfig.Builder("jdbc:mysql://mysql-common.qax-pub.yqn.corp:3306/yqn_composer_v2", "root", "sr@12345"))
             .globalConfig(builder -> builder.author("zhujunming@yunquna.com")
                 .enableSwagger()
                 .outputDir("E://code")
@@ -24,9 +26,14 @@ public class YqnGeneratorTest {
                 .entity("dao.domain")
                 .mapper("dao.mapper")
                 .pathInfo(Collections.singletonMap(OutputFile.xml, "E://code/mapper")))
-            .strategyConfig(builder -> builder.addInclude("composer_helper_doc")
+            //表名
+            .strategyConfig(builder -> builder.addInclude("composer_commit_log")
                 .entityBuilder()
-                .enableLombok())
+                .enableLombok()
+                .mapperBuilder()
+                //控制生成resultMap
+                .enableBaseResultMap()
+                .enableBaseColumnList())
             .templateEngine(new FreemarkerTemplateEngine())
             .execute();
     }
